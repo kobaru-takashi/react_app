729fb8b277185e33efb9b64e29eaeea77879d78a
kobaru-takashi <ta.a.1445.1@gmail.com>
2020-10-05 21:45:23 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,40 +1,33 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import './App.css';
-import Memo from './memo/Memo';
-import AddForm from './memo/AddForm';
-import FindForm from './memo/FindForm';
-import DelForm from './memo/DelForm';
-import PersistForm from './memo/PersistForm';
+import Sampledata from './fire/SampleData';
+import firebase from "firebase";
+
+
+// Firebaseの設定
+
+
+
+// Firebaseの初期化
+firebase.initializeApp(config);
+
 
 // Appコンポーネント
 class App extends Component {
-  td = {
-    width:"250px"
-  }
 
-
-  constructor(props){
-    super(props);
-  }
 
   render() {
     return (
       <div>
-        <h1>Memo</h1>
-        <AddForm />
-        <hr />
-        <table><tbody><tr>
-          <td style={this.td}><FindForm /></td>
-          <td style={this.td}><DelForm /></td>
-          <td style={this.td}><PersistForm /></td>
-        </tr></tbody></table>
-        <Memo />
+        <h1>Fire</h1>
+        <h2>Sample data.</h2>
+        <Sampledata />
       </div>
     );
-  }  
+  }
 }
 
 
-export default connect()(App);
+export default App;
+
 
